df1040d5f8689439408bbbaf7ea77b0db92957ed
AnubisRuby <cristian.audir8@hotmail.com>
2024-03-14 00:59:00 -0500

    segundo commit

--- sisventas/sisventas/urls.py
+++ sisventas/sisventas/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-path('admin/', admin.site.urls),
-path('', include('plantilla.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('plantilla.urls')),
 ]
 urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
