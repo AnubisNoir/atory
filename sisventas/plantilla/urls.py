927260a48006367a04dc7b5810bfe1276c529f37
AnubisRuby <cristian.audir8@hotmail.com>
2024-04-01 19:54:40 -0500

    funcino completa

--- sisventas/plantilla/urls.py
+++ sisventas/plantilla/urls.py
@@ -6,5 +6,7 @@ urlpatterns=[
     path('home', views.home, name='Home'),
     path('facturas', views.facturas, name='Factura'),
     path('usuarios', views.usuarios, name='Usuario'),
-
+    path('planes', views.planes, name='Plan'),
+    path('clientes', views.cliente, name='Cliente'),
+    
 ]
--- sisventas/plantilla/views.py
+++ sisventas/plantilla/views.py
@@ -9,3 +9,7 @@ def facturas (request):
     return render (request, 'facturas.html' )
 def usuarios (request):
     return render (request, 'usuarios.html')
+def planes (request):
+    return render (request, 'planes.html')
+def cliente (request):
+    return render (request, 'cliente.html')
